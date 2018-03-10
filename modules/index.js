8966883365dae0966d3be2962becf99f6f5f6ff3
Tyler Cvetan <tcvetan@connecture.com>
2018-03-10 12:11:46 -0600

    Adding index

---
+++ modules/index.js
@@ -0,0 +1,5 @@
+import { list as aList } from './attractions';
+import { list as dList } from './dinning';
+export const attractions = { aList };
+export const dining = { dList };
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { list  as aList } from './attractions';
+import { list as dList } from './dinning';
+
+export const attractions = { aList };
+
+export const dining = { dList }; 
