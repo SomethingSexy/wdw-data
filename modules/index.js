f41a4ebd8b7e4620547af237f835156cd14748f1
Tyler Cvetan <tcvetan@connecture.com>
2018-03-20 20:38:13 -0500

    Exporting hours

--- modules/index.js
+++ modules/index.js
@@ -2,6 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const attractions_1 = require("./attractions");
 const dinning_1 = require("./dinning");
+const hours_1 = require("./hours");
 exports.attractions = { list: attractions_1.list };
 exports.dining = { list: dinning_1.list };
+// This is really hours by date, it could go in a calendar export
+// or tied to parks, however you might only want to get the calendar
+// in subsequent calls
+exports.hours = { list: hours_1.list };
+// TODO Add park information here
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { list  as aList } from './attractions';
 import { list as dList } from './dinning';
+import { list as hList } from './hours';
 
 export const attractions = { list: aList };
-
 export const dining = { list: dList };
+// This is really hours by date, it could go in a calendar export
+// or tied to parks, however you might only want to get the calendar
+// in subsequent calls
+export const hours = { list: hList };
+// TODO Add park information here
