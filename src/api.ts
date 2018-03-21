aed008b3d9247df4253117ed71c187cad6b6b2ed
Tyler Cvetan <tcvetan@connecture.com>
2018-03-20 19:32:26 -0500

    Adding api to retrieve park hours

---
+++ src/api.ts
@@ -0,0 +1,18 @@
+import 'fetch-everywhere';
+
+export default async (path: string) => {
+  const response = await fetch(`https://disneyworld.disney.go.com/${path}/`, {
+    headers: {
+      Accept: '*/*',
+      'Accept-Language': 'en-US,en;q=0.8',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      Host: 'disneyworld.disney.go.com',
+      Origin: 'https://disneyworld.disney.go.com',
+      'X-Requested-With': 'XMLHttpRequest',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36' // tslint:disable-line
+    },
+    method: 'get'
+  });
+
+  return response.text();
+};
---
+++ test/hours.ts
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import 'mocha';
+import { list } from '../src/hours';
+
+describe('hours', () => {
+  describe('list', () => {
+    it('should fetch latest park hours', async () => {
+      return list()
+        .then(response => {
+          expect(response.length > 0).to.equal(true);
+        });
+    });
+
+    it('should fetch multiple months', async () => {
+      return list(2)
+        .then(response => {
+          expect(response.length > 0).to.equal(true);
+        });
+    });
+  });
+});
